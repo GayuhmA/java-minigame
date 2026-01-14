07ef53a264fc8e0e88f91321bdbcf57a755ba01d
GayuhmA <gayuhmuktiaji9@gmail.com>
2026-01-14 23:30:47 +0700

    feat: implement Player, Weapon, Shield classes

---
+++ exercise/minigame.java
@@ -0,0 +1,64 @@
+class Player{
+    String name;
+    double health;
+    Weapon weapon;
+
+    Player(String name, double health){
+        this.name = name;
+        this.health = health;
+    }
+
+    void info(){
+        System.out.println("\n----- Player Info -----");
+        System.out.println("Player name: " + this.name);
+        System.out.println("Player health: " + this.health);
+        System.out.println("Player Weapon: " + this.weapon.name);
+    }
+
+    void equipWeapon(Weapon weapon){
+        this.weapon = weapon;
+    }
+}
+
+class Weapon{
+    String name;
+    double damage;
+
+    Weapon(String name, double damage){
+        this.name = name;
+        this.damage = damage;
+    }
+}
+
+class Shield{
+    String name;
+    double defense;
+
+    Shield(String name, double defense){
+        this.name = name;
+        this.defense = defense;
+    }
+}
+
+public class minigame {
+    public static void main(String[] args) {
+        // player setup
+        Player player1 = new Player("Robert", 100);
+        Player player2 = new Player("Ginko", 100);
+
+        // weapon setup
+        Weapon knife = new Weapon("Knife", 5);
+        Weapon sword = new Weapon("Sword", 10);
+
+        // shield setup
+        Shield iron = new Shield("Iron", 20);
+        Shield rock = new Shield("Rock", 10);
+
+        // player action
+        player1.equipWeapon(knife);
+
+        // player info
+        player1.info();
+
+    }
+}
